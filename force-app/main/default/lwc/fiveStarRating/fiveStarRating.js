8c3faa014d7e5fc3cf045171417b245f9983652c
mikesobczak <msobczak@yahoo.com>
2022-04-22 09:38:08 -0400

    challenge 11 after

--- force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
+++ force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
@@ -1,17 +1,27 @@
+import { LightningElement, api } from "lwc";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { loadScript, loadStyle } from "lightning/platformResourceLoader";
 //import fivestar static resource, call it fivestar
+import fivestar from "@salesforce/resourceUrl/fivestar";
 
 // add constants here
+const ERROR_TITLE = 'Error loading five-star';
+const ERROR_VARIANT = 'error';
+const EDITABLE_CLASS = 'c-rating';
+const READ_ONLY_CLASS = 'readonly c-rating';
 
 export default class FiveStarRating extends LightningElement {
   //initialize public readOnly and value properties
-  readOnly;
-  value;
+  @api readOnly;
+  @api value;
 
   editedValue;
   isRendered;
 
   //getter function that returns the correct class depending on if it is readonly
-  starClass() {}
+  get starClass() {
+    return this.readOnly ? READ_ONLY_CLASS : EDITABLE_CLASS;
+  }
 
   // Render callback to load the script once the component renders.
   renderedCallback() {
@@ -25,7 +35,24 @@ export default class FiveStarRating extends LightningElement {
   //Method to load the 3rd party script and initialize the rating.
   //call the initializeRating function after scripts are loaded
   //display a toast with error message if there is an error loading script
-  loadScript() {}
+  loadScript() {
+    Promise.all([
+      loadScript(this, fivestar + '/rating.js'),
+      loadStyle(this, fivestar + '/rating.css')
+  ])
+      .then(() => {
+          this.initializeRating();
+      })
+      .catch(error => {
+          this.dispatchEvent(
+              new ShowToastEvent({
+                  title: ERROR_TITLE,
+                  message: error.message,
+                  variant: ERROR_VARIANT
+              })
+          );
+      });
+  }
 
   initializeRating() {
     let domEl = this.template.querySelector('ul');
@@ -46,5 +73,11 @@ export default class FiveStarRating extends LightningElement {
 
   // Method to fire event called ratingchange with the following parameter:
   // {detail: { rating: CURRENT_RATING }}); when the user selects a rating
-  ratingChanged(rating) {}
+  ratingChanged(rating) {
+    const evt = new CustomEvent("ratingchange", {
+      detail: { rating: CURRENT_RATING },
+      bubbles: true
+    });
+    this.dispatchEvent(evt);
+  }
 }
